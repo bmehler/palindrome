937eedeeafdc00270491a886f5ca4c8acb62b7db
bmehler <bernhard.mehler@gmail.com>
2014-05-31 14:03:01 +0200

    Sniffer Errors fixed

--- Palindrom.php
+++ Palindrom.php
@@ -2,19 +2,21 @@
 
 class Palindrom
 {
-    private $wort;  
+    private $_wort;  
 
-    public function __construct() {}    
+    public function __construct() 
+    {
+    }    
     
     public function isPalindrom ($wort)
     {             
-        if(!is_string($wort))
+        if (!is_string($wort))
         {
             throw new \InvalidArgumentException('Invalid argument');            
         } else {        
             $originalString = strtolower($wort);       
             $reversedString = strrev($originalString);           
-            if(strcmp($originalString, $reversedString) == 0) {
+            if (strcmp($originalString, $reversedString) == 0) {
                 return TRUE;
             } else {
                 return FALSE;
@@ -22,4 +24,3 @@ class Palindrom
         }
     }
 }
-?>
