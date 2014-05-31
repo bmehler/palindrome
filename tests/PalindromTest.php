adc8bc4938ad8110a6fe792d618540e502fdeeb4
bmehler <bernhard.mehler@gmail.com>
2014-05-31 22:49:41 +0200

    sniffer fixed

--- tests/PalindromTest.php
+++ tests/PalindromTest.php
@@ -1,6 +1,5 @@
 ﻿<?php
-class PalindromTest extends PHPUnit_Framework_TestCase
-{  
+class PalindromTest extends PHPUnit_Framework_TestCase {  
     /** 
      * @dataProvider isValidPalindrom
      * @test
