ac4ab0a53aef4aaa82178989500371393e024693
bmehler <bernhard.mehler@gmail.com>
2014-05-31 22:32:11 +0200

    static method for autoloader

---
+++ tests/autoloader.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+class autoloader
+{
+    public static function loader()
+    {
+           spl_autoload_register('self::load');
+    }
+    
+    public static function load($class)
+    {
+        $file = $class . '.php';
+        
+        if (file_exists($file))
+        {
+            require $file;
+        }
+    }
+}
+
+autoloader::loader();    
