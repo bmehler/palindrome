c39dc96f8510246b71a09efe40f5a540e3e8bb97
bmehler <bernhard.mehler@gmail.com>
2014-06-01 21:25:36 +0200

    changed sniffer default standard

--- tests/autoloader.php
+++ tests/autoloader.php
@@ -1,12 +1,33 @@
 ﻿<?php
-
-class autoloader
+/**
+ * Represents a Autoloader Class for including classes.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   palindrome
+ * @author    Bernhard Mehler <bernhard.mehler@gmail.com>
+ * @copyright 2014 Bernhard Mehler
+ * @license   GPL
+ * @link      git://github.com/bmehler/palindrome.git bmehler/palindrome
+ */
+class Autoloader
 {
+    /**
+     * Calls the static function load with the current class for autoloading.
+     *
+     * @return void
+     */
     public static function loader()
     {
-           spl_autoload_register('self::load');
+        spl_autoload_register('self::load');
     }
     
+    /**
+     * Includes the current class and do a require.
+     *
+     * @return void
+     */
     public static function load($class)
     {
         $file = $class . '.php';
@@ -17,4 +38,4 @@ class autoloader
     }
 }
 
-autoloader::loader();    
+Autoloader::loader();    
